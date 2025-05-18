45126de0aa90244ca71280c43ba6420d828d3691
ramadan <jaafariramadan@gmail.com>
2025-05-18 11:08:58 +0300

    Builder Pattern Inheretence issue

---
+++ builder/html-builder/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPatterns {
+    public class Person
+    {
+        public string Name;
+        public string Position;
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class PersonInfoBuilder {
+        protected Person person = new Person();
+
+        public PersonInfoBuilder Called(string name) {
+            person.Name = name;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonInfoBuilder {
+        public PersonJobBuilder WorkAsA(string position) {
+            person.Position = position;
+            return this;
+        }
+    }
+
+    internal class Program {
+        public static void Main(string[] args) {
+            PersonJobBuilder builder = new PersonJobBuilder();
+            builder.Called("Ahmed").WorkAsA("Farmer");
+        }
+    }
+}
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
